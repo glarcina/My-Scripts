15c8d111129e772faaf40b4716d961e71bda1062
glarcina <greg.larcina@gmail.com>
2018-10-04 09:48:50 -0400

    Add TODO for handling multiple VPN networks

--- Scripts/adaptvpn.sh
+++ Scripts/adaptvpn.sh
@@ -4,6 +4,12 @@
 vpncert=/etc/openvpn/vpnloc/virylsw.ovpn
 
 # Function to (dis)connect to Viryl's ADAPT VPN
+#
+# TODO: This script uses a killall command to disconnect from our one and
+# 	only VPN network.  If you need to connect to multiple VPN's you'll
+#	want to change this to be able to specify which network to control.
+#	Likely will have to incorporate iptables + bash script.
+# https://askubuntu.com/questions/589477/is-there-a-vpn-monitor-kill-switch-application-for-ubuntu
 adaptvpn () {
 
     NC='\033[0m' # No Color
